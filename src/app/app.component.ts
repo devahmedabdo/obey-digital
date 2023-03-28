44de05fbeb5cec533a56a196bd151e176e4de00f
Ahmed Abdo <devahmedabdo@gmail.com>
2023-03-28 16:51:28 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'obey-digital';
+  activeMenu: boolean = false;
+  reflectActiveMenu() {
+    this.activeMenu = !this.activeMenu;
+  }
+  services: any = [
+    {
+      name: 'FACEBOOK ADS',
+      icon: 'facebook.svg',
+      dis: `From awareness to conversion, our 
+      Facebook ads help you reach your 
+      target audience and build your 
+      dental practice like never before.`,
+    },
+    {
+      name: 'leads',
+      icon: 'user.svg',
+      dis: `Get more patients in the door with 
+      our lead generation solutions 
+      tailored to the dental industry 
+      and designed to drive real results.`,
+    },
+  ];
+  ngOnInit(): void {
+    window.addEventListener('scroll', () => {
+      this.activeMenu = false;
+    });
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#15A3C7",
+        lPrimary: "#E8F6F9",
+      },
+    },
+  },
+  plugins: [],
+};
